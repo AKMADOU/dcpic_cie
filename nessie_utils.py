197416fca934508e820d3dfcda289fbe6b53fe17
ADOU Kouamé Mathurin <82063354+AKMADOU@users.noreply.github.com>
2025-09-18 07:57:11 +0000

    Update nessie_utils.py

--- nessie_utils.py
+++ nessie_utils.py
@@ -2,7 +2,7 @@ import os
 import duckdb
 import sqlalchemy as sa
 from urllib.parse import quote_plus
-from dcpic_cie.config import POSTGRES_CONFIG, NESSIE_CONFIG
+from config import POSTGRES_CONFIG, NESSIE_CONFIG
 
 def push_df_to_nessie(df, pg_table_name, trino_table_name,
                       pg_user=None, pg_password=None,
